cff6c435f746b60c0dc03645fa400fb70d535f28
abboskhan <abbosxon28012004@gmail.com>
2023-02-07 20:21:22 +0500

    failed

--- lesson3/src/main/resources/SOLUTION.sql
+++ lesson3/src/main/resources/SOLUTION.sql
@@ -1,17 +1,17 @@
-ALTER TABLE student alter column birthday Date NOT NULL;
+ALTER TABLE student ALTER COLUMN birthday DATE NOT NULL;
 
-alter table mark add check ( grade>=1 and grade<=10 )
+ALTER TABLE mark ADD CHECK (mark >= 1 AND mark <= 10);
 
-alter table mark alter column student_id BIGINT not null ;
+ALTER TABLE mark ALTER COLUMN student_id BIGINT NOT NULL;
 
-alter table mark alter column subject_id BIGINT not null ;
+ALTER TABLE mark ALTER COLUMN subject_id BIGINT NOT NULL;
 
-alter table subject add check ( grade>=1 and grade<=5)
+ALTER TABLE subject ADD CHECK (grade >= 1 AND grade <= 5);
 
-ALTER TABLE paymenttype ADD UNIQUE (name);
+ALTER TABLE paymenttype ADD UNIQUE(NAME);
 
-alter table payment alter column type_id BIGINT not null ;
+ALTER TABLE payment ALTER COLUMN type_id BIGINT NOT NULL;
 
-alter table payment alter column amount NUMERIC not null ;
+ALTER TABLE payment ALTER COLUMN amount NUMERIC NOT NULL;
 
-alter table payment alter column payment_date DateTime not null ;
+ALTER TABLE payment ALTER COLUMN payment_date TIMESTAMP NOT NULL;
