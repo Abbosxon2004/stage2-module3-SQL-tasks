87ff78f0799cef35f4c1c29f53fa189d4c7dfefd
abboskhan <abbosxon28012004@gmail.com>
2023-02-07 18:58:08 +0500

    thirdTaskCompleted

--- lesson3/src/main/resources/CREATEDB.sql
+++ lesson3/src/main/resources/CREATEDB.sql
@@ -1,42 +1,44 @@
 CREATE TABLE student
 (
-    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(45),
-    birthday DATE,
-    groupnumber INT NOT NULL
+    id          BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name        VARCHAR(45),
+    birthday    DATE   not null,
+    groupnumber INT    NOT NULL
 );
 CREATE TABLE subject
 (
-    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(250),
+    id          BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name        VARCHAR(250),
     description VARCHAR(255),
-    grade int NOT NULL
+    grade       int    NOT NULL,
+    check ( grade>=1 and grade<=5)
 );
 
 CREATE TABLE mark
 (
-    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    student_id BIGINT,
-    subject_id BIGINT,
-    mark INT,
-    foreign key (student_id) references student(id),
-    foreign key (subject_id) references subject(id)
+    id         BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    student_id BIGINT not null,
+    subject_id BIGINT not null,
+    mark       INT,
+    foreign key (student_id) references student (id),
+    foreign key (subject_id) references subject (id),
+    check ( mark >= 1 and mark <= 10 )
 );
 
 CREATE TABLE paymenttype
 (
-    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(45)
+    id   BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(45) unique
 );
 
 CREATE TABLE payment
 (
-    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    type_id BIGINT,
-    amount decimal,
-    payment_date DateTime,
-    student_id BIGINT NOT NULL,
-    foreign key (type_id) references paymenttype(id),
-    foreign key (student_id) references student(id)
+    id           BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    type_id      BIGINT not null ,
+    amount       decimal not null ,
+    payment_date DateTime not null ,
+    student_id   BIGINT NOT NULL,
+    foreign key (type_id) references paymenttype (id),
+    foreign key (student_id) references student (id)
 );
 
