2c69d65e8bf2f3882c2e7f09b3d14f58818563c8
abboskhan <abbosxon28012004@gmail.com>
2023-02-07 22:38:40 +0500

    changed5

--- lesson5/src/main/resources/SOLUTION.sql
+++ lesson5/src/main/resources/SOLUTION.sql
@@ -1,14 +1,14 @@
 select * from payment where amount>=500;
 
-select * from student where DATEadd(YEAR,-20,GETDATE())<=birthday;
+select * from student where DATEADD(YEAR,-20,GETDATE())<birthday;
 
-select * from student where DATEDIFF(YEAR,-20,GETDATE())>=birthday and groupnumber=10;
+select * from student where DATEADD(YEAR,-20,GETDATE())>birthday and groupnumber=10;
 
 Select * from student where name='Mike' or groupnumber in (4, 5, 6);
 
-select * from payment where DATEDIFF(MONTH,-8,GETDATE())>=payment_date;
+select * from payment where DATEADD(MONTH,-8,GETDATE())>=payment_date;
 
 select * from student  where name like 'A%';
 
-select * from student where name='Roxi' and groupnumber=4 or name='Tallie' and groupnumber=9;
+select id,name,birthday,groupnumber from student where (name='Roxi' and groupnumber=4) or (name='Tallie' and groupnumber=9);
 
