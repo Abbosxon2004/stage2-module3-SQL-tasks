86a95d0e153c9fe151e4e5311f815729ee7d0280
abboskhan <abbosxon28012004@gmail.com>
2023-02-09 10:23:11 +0500

    endingsAll

--- lesson1/src/main/resources/SOLUTION.sql
+++ lesson1/src/main/resources/SOLUTION.sql
@@ -1,36 +1,37 @@
-create table Student
-(
-    id       bigint primary key,
-    name     varchar,
-    birthday date,
-    groupnumber int
-);
-create table Subject
-(
-    id bigint primary key,
-    name varchar,
-    description varchar,
-    grade int
-);
-create table PaymentType(
-    id bigint primary key,
-    name varchar
-);
-create table Payment(
-    id bigint primary key,
-    type_id bigint,
-    FOREIGN KEY (type_id) references PaymentType(id),
-    amount decimal,
-    student_id bigint,
-    FOREIGN KEY (student_id) references student(id),
-    payment_date datetime
+CREATE TABLE student (
+                         id          BIGINT NOT NULL auto_increment PRIMARY KEY,
+                         name        VARCHAR(45),
+                         birthday    DATE NOT NULL,
+                         groupnumber INT NOT NULL
 );
 
-create table Mark(
-    id bigint primary key,
-    student_id bigint,
-    FOREIGN KEY (student_id) references Student(id),
-    subject_id bigint,
-    FOREIGN KEY (subject_id) references Subject(id),
-    mark int
-)
+CREATE TABLE subject (
+                         id          BIGINT NOT NULL auto_increment PRIMARY KEY,
+                         name        VARCHAR(250),
+                         description VARCHAR(255),
+                         grade       INT NOT NULL
+);
+
+CREATE TABLE mark (
+                      id         BIGINT NOT NULL auto_increment PRIMARY KEY,
+                      student_id BIGINT,
+                      subject_id BIGINT,
+                      mark       INT NOT NULL,
+                      FOREIGN KEY (student_id) REFERENCES student(id),
+                      FOREIGN KEY (subject_id) REFERENCES subject(id)
+);
+
+CREATE TABLE paymenttype (
+                             id   BIGINT NOT NULL auto_increment PRIMARY KEY,
+                             name VARCHAR(45)
+);
+
+CREATE TABLE payment (
+                         id           BIGINT NOT NULL auto_increment PRIMARY KEY,
+                         type_id      BIGINT NOT NULL,
+                         amount       DECIMAL NOT NULL,
+                         payment_date TIMESTAMP NOT NULL,
+                         student_id   BIGINT NOT NULL,
+                         FOREIGN KEY (type_id) REFERENCES paymenttype(id),
+                         FOREIGN KEY (student_id) REFERENCES student(id)
+);
